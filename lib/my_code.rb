bd3b3f7a7dd77059474cde1358aa201272b33f23
Aidan K McBride <aidankmcbride@gmail.com>
2020-03-04 20:22:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,11 @@
 def map(array) 
 newArr = [] 
 i = 0 
-while i < array.length 
-yield(array[i])
+  while i < array.length 
+  newArr.push(yield(array[i]))
+  i += 1 
+  end
+  new 
 end
 
 def reduce(array, sv=nil) 
