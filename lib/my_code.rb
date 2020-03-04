78d2919fc47cb04247a71398aff625d61d7ed63f
Aidan K McBride <aidankmcbride@gmail.com>
2020-03-04 20:29:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,5 +12,12 @@ def reduce(array, sv=nil)
 i = 0 
 total = 0 
   while i < array.length 
-    
+    if sv=nil
+      total += yield(array[i])
+    else 
+      total += yield(array[sv])
+    end 
+   i += 1  
+  end
+    total
 end
