4cf3a3f9bcad67242be47a1b53a837177b189388
Aidan K McBride <aidankmcbride@gmail.com>
2020-03-04 20:19:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,25 +1,8 @@
 def map(array) 
-  newArr = [] 
-  i = 0 
-  while i < array.length 
-  newArr.push(yield(array[i]))
-  i += 1 
-end
-newArr
+newArr = [] 
+
 end
 
 def reduce(array, sv=nil) 
-  if sv 
-    sum = sv 
-    i = 0 
-  else 
-    sum = array[0]
-    i = 1 
-  end 
-  
-  while i < array.length 
-  sum = yield(sum, array[i])
-  i += 1 
-end 
-sum 
+
 end
